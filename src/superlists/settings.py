63af437572f0479e45e413274053e8ecd4b1dff9
SNeXT <snext2004@gmail.com>
2025-02-23 20:32:31 +0700

    add csrf

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -21,6 +21,7 @@ if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+    CSRF_TRUSTED_ORIGINS = [f"https://{os.environ['DJANGO_ALLOWED_HOST']}"]
 else:
     DEBUG = True
     SECRET_KEY = "insecure-key-for-dev"
